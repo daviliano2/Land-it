438e2bb7e6bbf457ae858d97fcfd9c09c5a72303
daviliano2 <daviliano2@gmail.com>
2020-10-20 21:36:35 +0200

    video 43. Add script for ship and input for main keys

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("rotating right");
+        }
+    }
+}
