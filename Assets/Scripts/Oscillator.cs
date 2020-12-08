0eccf0767d9b23d199fe65797f73bbb4ff132556
daviliano2 <daviliano2@gmail.com>
2020-12-08 21:28:59 +0100

    video 61. Create level 3. Add obstacle and script to move it manually.

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [Range(0,1)] [SerializeField] float movementFactor;
+
+    Vector3 startingPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
